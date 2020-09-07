2620c8feaceb2d21b4266ac0065e191184df010a
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-09-07 03:30:25 +0100

    Changed the MongoDB database

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://rorisjack:catrapim123@cluster0.ncrey.mongodb.net/personal-website?retryWrites=true&w=majority'
+  mongoURI: 'mongodb://localhost:27017/blog'
 }
