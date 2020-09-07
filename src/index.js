a05c7b57aa683e524d778c395eb74881ee21cb76
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-09-07 03:30:37 +0100

    Updated article route

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+
+// Set view engine
+app.set('view engine', 'ejs');
 
 // Bodyparser Middleware
 app.use(express.json());
+
+// URL Encoder
+app.use(express.urlencoded({ extended: false }));
 
 // DB Config
 const db = require('../config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('Connected to DB'))
   .catch(err => console.log(err));
 
+// Articles
+const Article = require('./models/Article');
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc'
+  })
+  res.render('articles/index', { articles: articles })
+});
+
 // Use Routes
-const postRoutes = require('./routes/posts')
-app.use('/posts', postRoutes);
+const articleRouter = require('./routes/articles');
+app.use('/articles', articleRouter);
 
 // Start Server and Port configuration
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`)
-})
+});
