6eba70026b88740d79389be3db07dc1d3399864e
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-09-11 01:31:00 +0100

    Worked on register route

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const router = express.Router()
+
+const User = require('./../models/User')
 
 // GET route for login
 router.get('/login', (req, res) => {
@@ -9,6 +12,22 @@ router.get('/login', (req, res) => {
 // GET route for register
 router.get('/register', (req, res) => {
   res.render('admin/register')
+})
+
+// POST route for register
+router.post('/register', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  let user = new User()
+  user.username = req.body.name
+  user.email = req.body.email
+  user.password = hashedPassword
+  try {
+    user = await user.save()
+    res.redirect('/admin/login')
+  } catch (err) {
+    res.redirect('/admin/register')
+    console.log(err)
+  }
 })
 
 module.exports = router
