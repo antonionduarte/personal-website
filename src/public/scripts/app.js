112cca49872ee26c65bd66e15ffa16da86ae87e5
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-09-16 02:41:58 +0100

    Started reworking the front-end of the blog

---
+++ src/public/scripts/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/scripts/particles.json', () => {
+  console.log('callback - particles.js config loaded');
+});
